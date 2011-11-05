21bb15d150e9859ec2201a242c320f61fc6c15ce
MakotoTheKnight <makotothedragon@gmail.com>
2011-11-05 23:26:30 +0000

    - Fixed errors with duplicate finding.  What's left:  Populating the Album table!

--- db/TestCases.sql
+++ db/TestCases.sql
@@ -1,66 +1,97 @@
-//PreCondition:  Vanilla Table
+/*PreCondition:  Vanilla Table */
 
-     INSERT INTO Song VALUES('C:/music/', 'Radiohead', '.mp3', 'Faust Arp', 
-		'New Age', 5, 'SomeAlbum', '128', '1995', 'August');
-//Post Condition:  Song should be inserted into table, with above values.
+INSERT INTO Song VALUES('C:/music/', 'Radiohead', '.mp3', 'Faust Arp', 'New Age', 5, 'SomeAlbum', '128', '1995', 'August');
+/* Post Condition:  Song should be inserted into table, with above values.*/
 
 
+/*
 //PreCondition:  1 song in table
+*/
 
 INSERT INTO Song VALUES('C:/music/', 'Philip Glass', '.wav', 'Violin Concerto #3', 
 		'Orchestra', 7, 'Greatest of Philip Glass', '64', '2001', 'May');
 		
+/*
 //PreCondition:  2 songs in table
+*/
 INSERT INTO Song VALUES('C:/music/mymusic', '311', '.mp3', 'Come Original', 
 		'Punk', 12, 'Transition', '64', '1999', 'October');
+/*
 //PostCondition:  3 songs now in table
+*/
 
+/*
 //Precondition:  0 songs in favorites table
+*/
 INSERT INTO Favorites VALUES (INSERT INTO Song VALUES(2, 'C:/music/mymusic', 
 	'311', '.mp3', 'Come Original');
+/*
 //Postcondition:  1 song in favorites table
+*/
 
+/*
 //PreCondition:  3 songs in Song table (since each song has an album)
+*/
 SELECT * 
 FROM Song
+/*
 //PostCondition:  Same as prec.
+*/
 
+/*
 //PreCondition:  3 songs in Album table (since each song has an album)
+*/
 SELECT * 
 FROM Album
+/*
 //PostCondition:  Same as prec.
+*/
 
+/*
 //PreCondition:  3 songs in Favorites table (since each song has an album)
+*/
 SELECT * 
 FROM Favorites
+/*
 //PostCondition:  Same as prec.
+*/
 
+/*
 //PreCondition:  3 songs in table
+*/
 UPDATE Song SET bitrate ='128' WHERE location.song = 'C:/music/mymusic' AND
 	Artist.Song = '311' AND filetype.Song = '.mp3' AND title.Song = 
 	'Come Original');
+/*
 //PostCondition:  3 songs in table, song 'Come Original" is updated with new 
+*/
+/*
 //bitrate
+*/
 
+/*
 //PreCondition:  3 songs in Album table (since each song has an album)
+*/
 SELECT * 
 FROM Song
+/*
 //PostCondition:  Same as prec.
+*/
 
+/*
 //PreCondition:  3 songs in Album table (since each song has an album)
+*/
 SELECT * 
 FROM Album
+/*
 //PostCondition:  Same as prec.
+*/
 
+/*
 //PreCondition:  3 songs in Album table (since each song has an album)
+*/
 SELECT * 
 FROM Favorites
+/*
 //PostCondition:  Same as prec.
-
-
-
-
-
-
-
-
+*/
--- db/tables.sql
+++ db/tables.sql
@@ -75,100 +75,75 @@ CREATE TABLE album(
 
 /* Triggers */
 
-CREATE TRIGGER IF NOT EXISTS song.updateLocation AFTER UPDATE OF location on song FOR EACH ROW
-BEGIN (
+CREATE TRIGGER updateLocation AFTER UPDATE OF location on song FOR EACH ROW
+BEGIN
 	UPDATE favorites SET location = song.location WHERE favorites.location = old.location;
-	
 	UPDATE album SET location = song.location WHERE album.location = old.location;
-	
 	UPDATE duplicates SET location = song.location WHERE duplicates.location = old.location;
-	
 	UPDATE contains SET location = song.location WHERE contains.location = old.location;
-)
 END; 
 
-CREATE TRIGGER IF NOT EXISTS song.updateArtist AFTER UPDATE OF artist on song FOR EACH ROW
-BEGIN (
+CREATE TRIGGER updateArtist AFTER UPDATE OF artist on song FOR EACH ROW
+BEGIN
 	UPDATE favorites SET artist = song.artist WHERE favorites.artist = old.artist;
-	
 	UPDATE album SET artist = song.artist WHERE album.artist = old.artist;
-	
 	UPDATE duplicates SET artist = song.artist WHERE duplicates.artist = old.artist;
-	
 	UPDATE contains SET artist = song.artist WHERE contains.artist = old.artist;
-)
 END; 
 
-CREATE TRIGGER IF NOT EXISTS song.updateTitle AFTER UPDATE OF title on song FOR EACH ROW
-BEGIN (
+CREATE TRIGGER updateTitle AFTER UPDATE OF title on song FOR EACH ROW
+BEGIN
 	UPDATE favorites SET title = song.title WHERE favorites.title = old.title;
-	
 	UPDATE album SET title = song.title WHERE album.title = old.title;
-	
 	UPDATE duplicates SET title = song.title WHERE duplicates.title = old.title;
-	
 	UPDATE contains SET title = song.title WHERE contains.title = old.title;
-)
 END; 
 
-CREATE TRIGGER IF NOT EXISTS song.updateFiletype AFTER UPDATE OF filetype on song FOR EACH ROW
-BEGIN (
+CREATE TRIGGER updateFiletype AFTER UPDATE OF filetype on song FOR EACH ROW
+BEGIN
 	UPDATE favorites SET filetype = song.filetype WHERE favorites.filetype = old.filetype;
-	
 	UPDATE album SET filetype = song.filetype WHERE album.filetype = old.filetype;
-	
 	UPDATE duplicates SET filetype = song.filetype WHERE duplicates.filetype = old.filetype;
-	
 	UPDATE contains SET filetype = song.filetype WHERE contains.filetype = old.filetype;
-)
 END;
 
-CREATE TRIGGER IF NOT EXISTS song.deleteSong BEFORE DELETE ON song
-BEGIN (
+CREATE TRIGGER deleteSong BEFORE DELETE ON song
+BEGIN
 	DELETE FROM album
 	WHERE song.location = album.location and song.artist = album.artist and song.filetype = album.filetype and song.title = album.title;
-	
 	DELETE FROM favorites
 	WHERE song.location = favorites.location and song.artist = favorites.artist and song.filetype = favorites.filetype and song.title = favorites.title;
-	
 	DELETE FROM duplicates
 	WHERE song.location = duplicates.location and song.artist = duplicates.artist and song.filetype = duplicates.filetype and song.title = duplicates.title;
-	
 	DELETE FROM contains
 	WHERE song.location = contains.location and song.artist = contains.artist and song.filetype = contains.filetype and song.title = contains.title;
-)
 END;
 
-CREATE TRIGGER IF NOT EXISTS song.checkDuplicates AFTER INSERT ON song
+CREATE TRIGGER checkDuplicates AFTER INSERT ON song
 BEGIN
-	(
-		INSERT INTO duplicates (location, artist, filetype, title)
-		VALUES (
-			SELECT location, artist, filetype, title AS duplicate
-			FROM song
-			WHERE song.location = duplicate.location and song.artist = duplicate.artist and song.filetype = duplicate.filetype and song.title = duplicate.title;
-		)
-	)
+	INSERT INTO duplicates (location, artist, filetype, title)
+		SELECT location, artist, filetype, title
+		FROM song
+		WHERE artist = (SELECT artist from song)
+			and filetype = (SELECT filetype from song)
+			and title = (SELECT title from song)
+			and location <> (SELECT location from song);
 END;
 
 
-CREATE TRIGGER IF NOT EXISTS playlist.updateName AFTER UPDATE OF pname on playlist
+CREATE TRIGGER updateName AFTER UPDATE OF pname on playlist
 BEGIN
-	(
 	UPDATE contains SET pname = playlist.pname WHERE contains.pname = old.pname;
-)
 END;
 
-CREATE TRIGGER IF NOT EXISTS playlist.deleteName AFTER DELETE ON playlist
+CREATE TRIGGER deleteName AFTER DELETE ON playlist
 BEGIN
-	(
 	DELETE FROM contains WHERE contains.pname = old.pname;
-)
 END;
 
 /* Indexes/Indices*/
 
-CREATE INDEX IF NOT EXISTS KeyIndex ON song (location, artist, filetype, title);
-CREATE INDEX IF NOT EXISTS KeyIndex ON user (uid);
+CREATE INDEX KeyIndex ON song (location, artist, filetype, title);
+CREATE INDEX UserIndex ON user (uid);
 
 COMMIT;
