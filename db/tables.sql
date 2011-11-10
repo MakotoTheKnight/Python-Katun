fff8cccaf5d696d16f3de3729daf4df57dd1e99b
MakotoTheKnight <makotothedragon@gmail.com>
2011-11-10 03:10:12 +0000

    - Updated (and hopefully resolved) issue with triggers not working properly.

--- db/tables.sql
+++ db/tables.sql
@@ -79,49 +79,49 @@ CREATE TABLE album(
 
 CREATE TRIGGER updateLocation AFTER UPDATE OF location on song FOR EACH ROW
 BEGIN
-	UPDATE favorites SET location = song.location WHERE favorites.location = old.location;
-	UPDATE album SET location = song.location WHERE album.location = old.location;
-	UPDATE duplicates SET location = song.location WHERE duplicates.location = old.location;
-	UPDATE contains SET location = song.location WHERE contains.location = old.location;
+	UPDATE favorites SET location = new.location WHERE favorites.location = old.location;
+	UPDATE album SET location = new.location WHERE album.location = old.location;
+	UPDATE duplicates SET location = new.location WHERE duplicates.location = old.location;
+	UPDATE contains SET location = new.location WHERE contains.location = old.location;
 END; 
 
 CREATE TRIGGER updateArtist AFTER UPDATE OF artist on song FOR EACH ROW
 BEGIN
-	UPDATE favorites SET artist = song.artist WHERE favorites.artist = old.artist;
-	UPDATE album SET artist = song.artist WHERE album.artist = old.artist;
-	UPDATE duplicates SET artist = song.artist WHERE duplicates.artist = old.artist;
-	UPDATE contains SET artist = song.artist WHERE contains.artist = old.artist;
+	UPDATE favorites SET artist = new.artist WHERE favorites.artist = old.artist;
+	UPDATE album SET artist = new.artist WHERE album.artist = old.artist;
+	UPDATE duplicates SET artist = new.artist WHERE duplicates.artist = old.artist;
+	UPDATE contains SET artist = new.artist WHERE contains.artist = old.artist;
 END; 
 
 CREATE TRIGGER updateTitle AFTER UPDATE OF title on song FOR EACH ROW
 BEGIN
-	UPDATE favorites SET title = song.title WHERE favorites.title = old.title;
-	UPDATE album SET title = song.title WHERE album.title = old.title;
-	UPDATE duplicates SET title = song.title WHERE duplicates.title = old.title;
-	UPDATE contains SET title = song.title WHERE contains.title = old.title;
+	UPDATE favorites SET title = new.title WHERE favorites.title = old.title;
+	UPDATE album SET title = new.title WHERE album.title = old.title;
+	UPDATE duplicates SET title = new.title WHERE duplicates.title = old.title;
+	UPDATE contains SET title = new.title WHERE contains.title = old.title;
 END; 
 
 CREATE TRIGGER updateFiletype AFTER UPDATE OF filetype on song FOR EACH ROW
 BEGIN
-	UPDATE favorites SET filetype = song.filetype WHERE favorites.filetype = old.filetype;
-	UPDATE album SET filetype = song.filetype WHERE album.filetype = old.filetype;
-	UPDATE duplicates SET filetype = song.filetype WHERE duplicates.filetype = old.filetype;
-	UPDATE contains SET filetype = song.filetype WHERE contains.filetype = old.filetype;
+	UPDATE favorites SET filetype = new.filetype WHERE favorites.filetype = old.filetype;
+	UPDATE album SET filetype = new.filetype WHERE album.filetype = old.filetype;
+	UPDATE duplicates SET filetype = new.filetype WHERE duplicates.filetype = old.filetype;
+	UPDATE contains SET filetype = new.filetype WHERE contains.filetype = old.filetype;
 END;
 
-CREATE TRIGGER deleteSong BEFORE DELETE ON song
+CREATE TRIGGER deleteSong BEFORE DELETE ON song FOR EACH ROW
 BEGIN
 	DELETE FROM album
-	WHERE song.location = album.location and song.artist = album.artist and song.filetype = album.filetype and song.title = album.title;
+	WHERE new.location = album.location and new.artist = album.artist and new.filetype = album.filetype and new.title = album.title;
 	DELETE FROM favorites
-	WHERE song.location = favorites.location and song.artist = favorites.artist and song.filetype = favorites.filetype and song.title = favorites.title;
+	WHERE new.location = favorites.location and new.artist = favorites.artist and new.filetype = favorites.filetype and new.title = favorites.title;
 	DELETE FROM duplicates
-	WHERE song.location = duplicates.location and song.artist = duplicates.artist and song.filetype = duplicates.filetype and song.title = duplicates.title;
+	WHERE new.location = duplicates.location and new.artist = duplicates.artist and new.filetype = duplicates.filetype and new.title = duplicates.title;
 	DELETE FROM contains
-	WHERE song.location = contains.location and song.artist = contains.artist and song.filetype = contains.filetype and song.title = contains.title;
+	WHERE new.location = contains.location and new.artist = contains.artist and new.filetype = contains.filetype and new.title = contains.title;
 END;
 
-CREATE TRIGGER checkDuplicates AFTER INSERT ON song
+CREATE TRIGGER checkDuplicates AFTER INSERT ON song FOR EACH ROW
 BEGIN
 	INSERT INTO duplicates (location, artist, filetype, title)
 		SELECT location, artist, filetype, title
@@ -133,12 +133,12 @@ BEGIN
 END;
 
 
-CREATE TRIGGER updateName AFTER UPDATE OF pname on playlist
+CREATE TRIGGER updateName AFTER UPDATE OF pname on playlist FOR EACH ROW
 BEGIN
 	UPDATE contains SET pname = playlist.pname WHERE contains.pname = old.pname;
 END;
 
-CREATE TRIGGER deleteName AFTER DELETE ON playlist
+CREATE TRIGGER deleteName AFTER DELETE ON playlist FOR EACH ROW
 BEGIN
 	DELETE FROM contains WHERE contains.pname = old.pname;
 END;
@@ -147,5 +147,6 @@ END;
 
 CREATE INDEX KeyIndex ON song (location, artist, filetype, title);
 CREATE INDEX UserIndex ON user (uid);
+CREATE INDEX SongIndex on song(genre, track, album,	bitrate);
 
 COMMIT;
