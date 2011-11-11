9ae961b86f8b7ee968a18ab0955bf34184955a53
Anon <anon@ymo.us>
2011-11-11 00:36:15 +0000

    More Duplicate tests

--- db/DupCases.sql
+++ db/DupCases.sql
@@ -23,9 +23,22 @@ INSERT INTO Song VALUES('C:/music/MyMusic/Songs', 'Philip Glass', 'wav', 'Violin
 		'Orchestra', 7, 'Greatest of Philip Glass', 64, 2001, 'May');
 --PostCondition:  2 songs in Song
 
+INSERT INTO Song VALUES('C:/Music/Song', 'Justin Bieber', '.mp3', 
+	'I''m Not the Father, I Swear', 'Pop', 5, 'Biebertastic', 128, 
+	2011, 'November');
+	
+	INSERT INTO Song VALUES('C:/Mine/Song', 'Justin Bieber', '.mp3', 
+	'I''m Not the Father, I Swear', 'Pop', 5, 'Biebertastic', 128, 
+	2011, 'November');
+
+SELECT *
+FROM Song;
 
 SELECT *
 FROM Duplicates;
+
+
+
 
 
 
