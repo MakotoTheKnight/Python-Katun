d2a1f312b9d55ba8ff70809ced6bbb7dcf27ccfa
MakotoTheKnight <makotothedragon@gmail.com>
2011-11-22 05:22:51 +0000

    - Main structure of site is good to go. - Work remaining: -> Tie-in rest of database -> Importing of parser -> Sanitization of database from within parser (or here, likely here)

--- katun-web/lib/__init__.py
+++ katun-web/lib/__init__.py
@@ -1,2 +1,2 @@
 import sys
-sys.path.append('lib')
+sys.path.append('lib/')
--- katun-web/web_backend.py
+++ katun-web/web_backend.py
@@ -4,22 +4,49 @@
 # This backend acts as an intermediary between the database and the end user, allowing for safe operations on the database.
 # The intention of this design is to mitigate potential SQL injections into the database, causing corruption and/or data loss.
 
-import lib.web as web
+import lib.web as web, exceptions
 
 __metaclass__ = type
 
-class HTMLInterface:
-	'''Specify the HTML specific interface to the website.
-	We allow our structure to appear like this:
+urls = (
+	'/', 'Index',
+	'/music', 'Music',
+	'/music/duplicates', 'Duplicates',
+	'/playlists', 'Playlists',
+	'/favorites', 'Favorites'
 	
-	/
-	--/music
-	----/duplicates
-	--/playlists
-	--/favorites'''
+)
+app = web.application(urls, globals())
+
+class Index:
+	'''Define the index class.  We'll focus on making it pretty later.'''
 	
-	def __init__(self):
-		pass
+	def GET(self):
+		return "Hello world!"
+		
+class Music:
+	'''Define the Music class.  We'll focus on making it pretty later.'''
+	
+	def GET(self):
+		return "Hello warld!"
+		
+class Duplicates:
+	'''Define the Duplicates class.  We'll focus on making it pretty later.'''
+	
+	def GET(self):
+		return "Hello werld!"
+		
+class Playlists:
+	'''Define the Playlists class.  We'll focus on making it pretty later.'''
+	
+	def GET(self):
+		return "Hello wurld!"
+		
+class Favorites:
+	'''Define the Favorites class.  We'll focus on making it pretty later.'''
+	
+	def GET(self):
+		return "Hello woorld!"
 
 
 class DatabaseInterface:
@@ -32,7 +59,13 @@ class DatabaseInterface:
 		self.db = db
 
 
-class HTMLError(Exeception):
+class HTMLError(Exception):
 	'''Specify a flexible HTML error.
 	The sorts of errors recognized are 404 and 403.'''
 	pass
+
+def main():
+	app.run()
+	
+if __name__ == '__main__':
+	main()
