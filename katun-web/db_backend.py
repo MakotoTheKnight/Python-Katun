d7f3449cdf0ac8f449e4e8a0cdefe7b5527429e1
MakotoTheKnight <makotothedragon@gmail.com>
2011-11-25 04:19:29 +0000

    - Added reset_database and build_database methods to DatabaseInterface.

--- katun-web/db_backend.py
+++ katun-web/db_backend.py
@@ -6,6 +6,8 @@
 
 __all__ = ['DatabaseInterface']
 
+import sqlite3, os
+
 class DatabaseInterface:
 	'''Specify the SQLite backend interface to the website.
 	
@@ -19,15 +21,11 @@ class DatabaseInterface:
 
 	@staticmethod
 	def reset_database():
-		'''Reset the database for new entries, or to clear something out.  Should only be used with the greatest of care.'''
-		self.cursor.execute("DROP TABLE song")
-		self.cursor.execute("DROP TABLE favorites")
-		self.cursor.execute("DROP TABLE user")
-		self.cursor.execute("DROP TABLE playlist")
-		self.cursor.execute("DROP TABLE album")
-		self.cursor.execute("DROP TABLE duplicates")
-		self.cursor.execute("DROP TABLE contains")
-		self.connect.commit()
+		'''"Reset" the database. 
+
+		This operation deletes the db file associated with K'atun.'''
+		
+		os.remove(self.db_path)
 		DatabaseInterface.build_database()
 
 	@staticmethod
@@ -35,7 +33,239 @@ class DatabaseInterface:
 		"""Rebuild the database for new entries.  Valid only after reset_database() is called, otherwise, undefined behavior
 		may occur."""
 		
-		with open("../tables.sql", 'r') as f:
-			pass
-			
+		self.connect = sqlite3.connect(self.db_path)
+		self.cursor = self.connect.cursor()
+		self.connect.execute("""P""")
 		
+		self.connect.executescript("""
+		
+		PRAGMA foreign_keys=ON;
+		BEGIN TRANSACTION;
+
+		/* Schemas */
+		CREATE TABLE song(
+			location TEXT,
+			artist TEXT,
+			filetype TEXT,
+			title TEXT,
+			genre TEXT,
+			track INTEGER,
+			album TEXT,
+			bitrate INTEGER,
+			year TEXT,
+			entryorder INTEGER,
+			--month INTEGER,
+			PRIMARY KEY(
+				location,
+				artist,
+				filetype,
+				title
+			)
+		);
+
+		CREATE TABLE user(
+			uid INTEGER PRIMARY KEY AUTOINCREMENT,
+			uname TEXT,
+			password TEXT
+		);
+
+		CREATE TABLE contains(
+			pname TEXT,
+			location TEXT,
+			artist TEXT,
+			filetype TEXT,
+			title TEXT,
+			FOREIGN KEY(pname) REFERENCES playlist(pname),
+			FOREIGN KEY(location, artist, filetype, title) REFERENCES song(location, artist, filetype, title)
+		);
+
+		CREATE TABLE favorites(
+			uid INTEGER,
+			location TEXT,
+			artist TEXT,
+			filetype TEXT,
+			title TEXT,
+			FOREIGN KEY(location, artist, filetype, title) references song(location, artist, filetype, title),
+			FOREIGN KEY(uid) REFERENCES user(uid)
+		);
+
+		CREATE TABLE duplicates(
+			location TEXT,
+			artist TEXT,
+			filetype TEXT,
+			title TEXT,
+			--duplicate_location TEXT PRIMARY KEY,
+			FOREIGN KEY(location, artist, filetype, title) references song(location, artist, filetype, title)
+		);
+
+		CREATE TABLE playlist(
+			pname TEXT PRIMARY KEY,
+			uid INTEGER,
+			count INTEGER,
+			FOREIGN KEY(uid) REFERENCES user(uid)
+		);
+
+		CREATE TABLE album(
+			location TEXT,
+			artist TEXT,
+			filetype TEXT,
+			title TEXT,
+			album_art_filename TEXT PRIMARY KEY,
+			track_count INTEGER,
+			FOREIGN KEY(location, artist, filetype, title) references song(location, artist, filetype, title)
+		);
+
+		/* Triggers */
+
+		CREATE TRIGGER updateLocation AFTER UPDATE OF location on song FOR EACH ROW
+		BEGIN
+			UPDATE favorites
+			SET location = new.location
+			WHERE favorites.location = old.location;
+			
+			UPDATE album
+			SET location = new.location
+			WHERE album.location = old.location;
+			
+			UPDATE duplicates
+			SET location = new.location
+			WHERE duplicates.location = old.location;
+			
+			UPDATE contains
+			SET location = new.location
+			WHERE contains.location = old.location;
+		END; 
+
+		CREATE TRIGGER updateArtist AFTER UPDATE OF artist on song FOR EACH ROW
+		BEGIN
+			UPDATE favorites
+			SET artist = new.artist
+			WHERE favorites.artist = old.artist;
+			
+			UPDATE album
+			SET artist = new.artist
+			WHERE album.artist = old.artist;
+			
+			UPDATE duplicates
+			SET artist = new.artist
+			WHERE duplicates.artist = old.artist;
+			
+			UPDATE contains
+			SET artist = new.artist
+			WHERE contains.artist = old.artist;
+		END; 
+
+		CREATE TRIGGER updateTitle AFTER UPDATE OF title on song FOR EACH ROW
+		BEGIN
+			UPDATE favorites
+			SET title = new.title 
+			WHERE favorites.title = old.title;
+			
+			UPDATE album
+			SET title = new.title 
+			WHERE album.title = old.title;
+			
+			UPDATE duplicates
+			SET title = new.title 
+			WHERE duplicates.title = old.title;
+			
+			UPDATE contains
+			SET title = new.title 
+			WHERE contains.title = old.title;
+		END; 
+
+		CREATE TRIGGER updateFiletype AFTER UPDATE OF filetype on song FOR EACH ROW
+		BEGIN
+			UPDATE favorites
+			SET filetype = new.filetype
+			WHERE favorites.filetype = old.filetype;
+			
+			UPDATE album
+			SET filetype = new.filetype
+			WHERE album.filetype = old.filetype;
+			
+			UPDATE duplicates
+			SET filetype = new.filetype
+			WHERE duplicates.filetype = old.filetype;
+			
+			UPDATE contains
+			SET filetype = new.filetype
+			WHERE contains.filetype = old.filetype;
+		END;
+
+		CREATE TRIGGER deleteSong BEFORE DELETE ON song FOR EACH ROW
+		BEGIN
+			DELETE FROM album
+			WHERE old.location = album.location
+			AND old.artist = album.artist
+			AND old.filetype = album.filetype
+			AND old.title = album.title;
+			
+			DELETE FROM favorites
+			WHERE old.location = favorites.location
+			AND old.artist = favorites.artist
+			AND old.filetype = favorites.filetype
+			AND old.title = favorites.title;
+			
+			DELETE FROM duplicates
+			WHERE old.location = duplicates.location
+			AND old.artist = duplicates.artist
+			AND old.filetype = duplicates.filetype
+			AND old.title = duplicates.title;
+			
+			DELETE FROM contains
+			WHERE old.location = contains.location
+			AND old.artist = contains.artist
+			AND old.filetype = contains.filetype
+			AND old.title = contains.title;
+		END;
+
+		CREATE TRIGGER checkDuplicates AFTER INSERT ON song FOR EACH ROW
+		BEGIN
+			INSERT INTO duplicates (location, artist, filetype, title)
+				SELECT location, artist, filetype, title
+				FROM song
+				WHERE artist = new.artist
+				AND filetype = new.filetype
+				AND title = new.title
+				AND location <> new.location
+				AND new.entryorder > entryorder;
+		END;
+
+		CREATE TRIGGER updateCount AFTER INSERT ON contains FOR EACH ROW
+		BEGIN
+			UPDATE playlist
+			SET count = (
+				SELECT count(*)
+				FROM contains
+				WHERE pname = contains.pname
+			);
+		END;
+
+
+		CREATE TRIGGER updateName AFTER UPDATE OF pname on playlist FOR EACH ROW
+		BEGIN
+			UPDATE contains
+			SET pname = new.pname
+			WHERE contains.pname = old.pname;
+		END;
+
+		CREATE TRIGGER deleteName AFTER DELETE ON playlist FOR EACH ROW
+		BEGIN
+			DELETE FROM contains
+			WHERE contains.pname = old.pname;
+		END;
+
+		/* Indexes/Indices*/
+
+		CREATE INDEX KeyIndex
+		ON song (location, artist, filetype, title);
+
+		CREATE INDEX UserIndex
+		ON user (uid);
+
+		CREATE INDEX SongIndex
+		ON song(genre, track, album, bitrate);
+
+		COMMIT;""")
+		self.connect.commit()
