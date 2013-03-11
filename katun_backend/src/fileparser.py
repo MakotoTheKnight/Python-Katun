2d7615478379359f4aa0e6faa53ee7d8e2273ed6
Jason Black <makotothedragon@gmail.com>
2013-03-10 23:37:31 -0600

    Working SQLAlchemy implementation

--- katun_backend/src/fileparser.py
+++ katun_backend/src/fileparser.py
@@ -13,18 +13,20 @@ supported = ['.ogg', '.flac', '.mp3']
 
 def load_directory(d):
     absolute_path = os.path.abspath(d)
-    for path, foldernames, files in os.walk(absolute_path):
+    for path, dirnames, files in os.walk(absolute_path):
         for f in files:
+            f = unicode(f)
             extension = os.path.splitext(f)
             if extension[1] in supported:
-                load_song_in_db(os.path.join(path, f), extension)
+                load_song_in_db(path, f, extension)
             else:
                 print "File not supported at this time, {}".format(f)
+        db.session.commit()
 
 
-def load_song_in_db(filename, ext):
+def load_song_in_db(filepath, filename, ext):
     try:
-        db.session.add(Song(datetime.today().isoformat(), filename, ext[-1].replace('.', '')))
+        db.session.add(Song(datetime.today().isoformat(), filepath, filename, ext[-1].replace('.', '')))
         db.session.commit()
     except Exception as e:
         print "WHAT THE HECK WENT WRONG.", e
